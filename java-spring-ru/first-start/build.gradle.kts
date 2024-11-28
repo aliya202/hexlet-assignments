b6b793602ddbf56f1c3219ed00879502a96553a3
Aliya <aakhisamova@gmail.com>
2024-11-28 12:45:47 +0400

    fixes

--- java-spring-ru/first-start/build.gradle.kts
+++ java-spring-ru/first-start/build.gradle.kts
@@ -22,9 +22,9 @@ repositories {
 
 dependencies {
     // BEGIN
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     // END
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
